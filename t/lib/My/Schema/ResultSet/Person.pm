69c9f3a06e871a2b7224c16e043089dd0f32bc58
Aaron Crane <cpan@aaroncrane.co.uk>
2015-12-16 15:02:16 +0000

    Import of ARC/DBIx-Class-ParameterizedJoinHack-0.001_001.tar.gz from CPAN.
    gitpan-cpan-distribution: DBIx-Class-ParameterizedJoinHack
    gitpan-cpan-version:      0.001_001
    gitpan-cpan-path:         ARC/DBIx-Class-ParameterizedJoinHack-0.001_001.tar.gz
    gitpan-cpan-author:       ARC
    gitpan-cpan-maturity:     developer

--- t/lib/My/Schema/ResultSet/Person.pm
+++
@@ -1,9 +0,0 @@
-package My::Schema::ResultSet::Person;
-
-use strict;
-use warnings;
-use base qw(DBIx::Class::ResultSet);
-
-__PACKAGE__->load_components(qw(ResultSet::ParameterizedJoinHack));
-
-1;
